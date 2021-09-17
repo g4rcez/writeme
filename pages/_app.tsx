985b94990ef194333e8d3f2ab944c6fcf6a93405
breno <bsromeiro@gmail.com>
2021-09-17 19:15:18 -0300

    add: header links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
+import { SiteContainer } from "components/container";
 import { SearchBar } from "components/search-bar";
 import type { AppProps } from "next/app";
-import { FormEvent, Fragment, useCallback, useEffect, useRef, useState } from "react";
-import Colors from "../styles/colors.json";
 import Link from "next/link";
-import "../styles/globals.css";
+import { FormEvent, Fragment, useCallback, useEffect, useRef, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { SiteContainer } from "components/container";
+import Colors from "../styles/colors.json";
+import "../styles/globals.css";
 
 const setColor = (varName: string, color: string, root: HTMLElement) => root.style.setProperty(varName, color);
 
@@ -42,16 +42,20 @@ export default function App({ Component, pageProps }: AppProps) {
         <SearchBar show={show} />
         <SiteContainer tag="nav" className="py-4 flex flex-nowrap items-center justify-between">
           <section className="flex items-baseline gap-x-8">
-            <h1 className="font-extrabold text-lg">WriteMe</h1>
+            <h1 className="font-extrabold text-lg">
+              <Link href="/">WriteMe</Link>
+            </h1>
             <ul className="flex gap-x-4 list-none">
               <li>
-                <Link href="/docs/">Docs</Link>
+                <Link href="/docs/project/playground/">Docs</Link>
               </li>
-              <li>Blog</li>
+              <li>
+                <Link href="/blog/">Blog</Link>
+              </li>
             </ul>
           </section>
           <form onSubmit={submit} className="flex gap-x-4 justify-between align-middle self-center items-center">
-            <h2>☀️</h2>
+            <h2 className="cursor-pointer">☀️</h2>
             <input
               ref={input}
               className="bg-gray-800 px-2 py-0.5 placeholder-shown:text-white rounded-lg text-base hidden md:block"

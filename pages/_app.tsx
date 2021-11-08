d540fff768a967dac6a3fc6fa666b91d612ef501
g4rcez <allan.f.garcez@gmail.com>
2021-11-07 23:44:06 -0300

    feat: database integration - finished

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 import ProgressBar from "@badrap/bar-of-progress";
+import { SessionProvider } from "next-auth/react";
 import { SiteContainer } from "components/container";
 import { SearchBar, ShortcutItem } from "components/search-bar";
 import { useDarkMode } from "hooks/use-dark-mode";
 //@ts-ignore
 import { useRemoteRefresh } from "next-remote-refresh/hook";
+import Image from "next/image";
 import type { AppProps } from "next/app";
+import { useSession, signIn, signOut } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { Router, useRouter } from "next/router";
-import { FormEvent, Fragment, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import React, { FormEvent, Fragment, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { FaSearch, FaSun } from "react-icons/fa";
 import { shortcutKeys } from "shortcut-keys";
 import "../styles/globals.css";
 import Light from "styles/themes/light.json";
+import { NextComponentType, NextPageContext } from "next";
+import { Is } from "lib/is";
 
 const progress = new ProgressBar({
   size: 3,
@@ -25,7 +31,22 @@ Router.events.on("routeChangeStart", () => progress.start);
 Router.events.on("routeChangeError", () => progress.finish());
 Router.events.on("routeChangeComplete", () => setTimeout(() => progress.finish(), 1000));
 
-export default function App({ Component, pageProps }: AppProps) {
+const LoggedIn = () => {
+  const session = useSession();
+  if (Is.NilOrEmpty(session.data)) {
+    return <button onClick={() => signIn()}>Sign in</button>;
+  }
+  if (Is.NilOrEmpty(session.data?.user)) {
+    return <button onClick={() => signIn()}>Sign in</button>;
+  }
+  return (
+    <div>
+      <img width="32px" src={session.data.user?.image!} alt="User avatar" className="rounded-full" />
+    </div>
+  );
+};
+
+function Content({ Component, pageProps }: { Component: NextComponentType<NextPageContext, any, {}>; pageProps: any }) {
   const router = useRouter();
   const input = useRef<HTMLInputElement>(null);
   const [show, setShow] = useState(false);
@@ -76,11 +97,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     const windowElement = shortcutKeys(window);
-    windowElement.add("control+z", onToggleMode);
-    windowElement.add("control+k", toggleSearchBar);
-    windowElement.add("control+h", () => goToPage("/"));
-    windowElement.add("control+p", () => goToPage("/docs/getting-started/"));
-    windowElement.add("escape", () => setShow(false));
+    const options = { multiPlatform: true, description: "shortcut", prevent: false };
+    windowElement.add("alt+m", onToggleMode, options);
+    windowElement.add("alt+k", toggleSearchBar, options);
+    windowElement.add("alt+h", () => goToPage("/"), options);
+    windowElement.add("alt+p", () => goToPage("/docs/getting-started/"), options);
+    windowElement.add("escape", () => setShow(false), options);
 
     return () => {
       windowElement.remove();
@@ -113,23 +135,35 @@ export default function App({ Component, pageProps }: AppProps) {
               </li>
             </ul>
           </section>
-          <form onSubmit={submit} className="flex gap-x-4 justify-between align-middle self-center items-center">
-            <button className="bg-transparent p-0 m-0" onClick={onToggleMode}>
-              <FaSun />
-            </button>
-            <input
-              ref={input}
-              className="bg-transparent px-2 py-0.5 placeholder-shown:text-main-accent border-neutral-slight border rounded border-opacity-20 text-base hidden md:block"
-              placeholder="Search...CTRL+K"
-              type="text"
-            />
-            <button onClick={toggleSearchBar} className="bg-transparent p-2 rounded-full text-base block md:hidden">
-              <FaSearch />
-            </button>
-          </form>
+          <div className="flex gap-x-4">
+            <form onSubmit={submit} className="flex gap-x-4 justify-between align-middle self-center items-center">
+              <button className="bg-transparent p-0 m-0" onClick={onToggleMode}>
+                <FaSun />
+              </button>
+              <input
+                ref={input}
+                className="bg-transparent px-2 py-0.5 placeholder-shown:text-main-accent border-neutral-slight border rounded border-opacity-20 text-base hidden md:block"
+                placeholder="Search...CTRL+K"
+                type="text"
+              />
+              <button onClick={toggleSearchBar} className="bg-transparent p-2 rounded-full text-base block md:hidden">
+                <FaSearch />
+              </button>
+            </form>
+            <LoggedIn />
+          </div>
         </SiteContainer>
       </header>
       <Component {...pageProps} />
     </Fragment>
+  );
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  const { session, ...componentProps } = pageProps;
+  return (
+    <SessionProvider session={session}>
+      <Content Component={Component} pageProps={componentProps} />
+    </SessionProvider>
   );
 }
--- prisma/migrations/20211106213658_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "document" ADD COLUMN     "description" TEXT NOT NULL DEFAULT E'';

59dc72137b52d3963cea9610a088b4fba2f44f3b
g4rcez <allan.f.garcez@gmail.com>
2022-11-04 20:16:03 -0300

    wip

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,22 +1,9 @@
 import FsSync from "fs";
 import Path from "path";
+import { WritemeRcConfig } from "src/writemerc";
 
 export namespace Config {
-  type RecursiveDict = {
-    [k: string]: string | RecursiveDict;
-  };
-
-  type ConfigValues = string | number | null | ConfigValues[];
-
-  export type Type = {
-    title: string;
-    baseUrl?: string;
-    strategy?: string;
-    defaultRepository?: string;
-    requestVariables?: Partial<Record<string, ConfigValues>>;
-  };
-
-  export const get = (): Type => {
+  export const get = (): WritemeRcConfig => {
     const path = Path.join(process.cwd(), "writeme.ts");
     const exists = FsSync.existsSync(path);
     if (!exists) return require(path);
@@ -24,6 +11,4 @@ export namespace Config {
   };
 
   export const properties = get();
-
-  export const define = (newConfig: Type) => newConfig;
 }
--- writeme.ts
+++ writeme.ts
@@ -1,7 +1,10 @@
-export default {
+import { config } from "src/writemerc";
+
+export default config({
+  title: "writeme",
   strategy: "fs",
   defaultRepository: "https://github.com/g4rcez/writeme",
   requestVariables: {
     host: "https://writeme.vercel.app",
   },
-};
+});
